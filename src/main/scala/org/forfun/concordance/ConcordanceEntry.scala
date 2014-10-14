64c4607ffcce6b4b59c6b13a42b751a68dc6cb72
mmswkod <mmswkod@gmail.com>
2014-10-14 16:29:43 -0400

    Fixing test

--- src/main/scala/org/forfun/concordance/ConcordanceEntry.scala
+++ src/main/scala/org/forfun/concordance/ConcordanceEntry.scala
@@ -1,3 +1,5 @@
 package org.forfun.concordance
 
-case class ConcordanceEntry(word: String, total: Int = 0, sentencesNumber: Seq[Int] = Seq.empty)
+case class ConcordanceEntry(word: String, total: Int = 0, sentencesNumber: Seq[Int] = Seq.empty){
+  override def toString() = s"$word {$total:${sentencesNumber.mkString(",")}}"
+}
